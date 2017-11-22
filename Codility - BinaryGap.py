e5fa831ee7933f5ba6bd111591d31af1ab389302
Jubin Soni <jsoni@converseon.com>
2017-11-22 15:41:38 -0500

    updated the code
    updated the code

--- Codility - BinaryGap.py
+++ Codility - BinaryGap.py
@@ -1,32 +1,49 @@
+#Python 3
+# @author: jubinsoni
+
 def binary(n, b):
-    if n<=0:
+    if n <= 0:
         return b
-    b = str(n%2) + b
-    return binary(n//2, b)
+    b = str(n % 2) + b
+    return binary(n // 2, b)
 
 
 def solution(N):
-	b = binary(N, b='')
-	binary_gap = -1
-	count = 0
+    b = binary(N, b='')
+    binary_gap = -1
+    count = 0
 
-	s = b[b.index('1')+1:]
-	for i in s:
-		if i == '1':
-			if binary_gap < count:
-				binary_gap = count
-			s = s[s.index('1')+1:]
-			
-			count = 0
-		else:
-			count +=1
+    s = b[b.index('1') + 1:]
+    for i in s:
+        if i == '1':
+            if binary_gap < count:
+                binary_gap = count
+            s = s[s.index('1') + 1:]
 
-	return binary_gap
-	
+            count = 0
+        else:
+            count += 1
+
+    return binary_gap
+
+
 def main():
-	N = 15
-	print(solution(N))
-	
-	N = 15
-	print(solution(N))
-	
+    # Test case#1
+    N = 15
+    print(solution(N))
+
+    # Test case#2
+    N = 541
+    print(solution(N))
+
+    # Test case#3
+    N = 0
+    print(solution(N))
+
+    # Test case#4
+    N = 241586698
+    print(solution(N))
+
+
+if __name__ == '__main__':
+    main()
